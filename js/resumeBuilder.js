4f5c514e8c5d507468c895a575f78945f4b6757a
bruno78 <devbruno78@gmail.com>
2016-10-14 12:52:23 -0400

    first commit

---
+++ js/resumeBuilder.js
@@ -0,0 +1,136 @@
+var bio = {
+  "name": "Bruno G. Tavares",
+  "role": "Full Stack Web Developer",
+  "contacts": {
+    "mobile": "347-927-1307",
+    "github": "bruno78",
+    "email": "me@brunogtavares.com",
+    "webpage": "http://brunogtavares.com"
+    "location": "Brooklyn NY"
+  },
+  "welcomeMessage": "Bruno's Resume"
+
+  "skills": [
+    "Ruby", "Python", "JavaScript", "R", "MySQL", "SQL"
+  ],
+  "bioPic": "images/fry.jpg"
+}
+
+var education = {
+  "schools": [
+    {
+      "name": "IEC - Pontificia Universidade Católica - MG",
+      "location": "Belo Horizonte, MG - Brazil",
+      "degree": "Post-Bachelors",
+      "minor": "Cinema",
+      "date": 2003,
+      "url": "http://www.pucminas.br/iec/destaques.php"
+    }
+
+    {
+      "name": "Pontificia Universidade Católica - MG",
+      "location": "Belo Horizonte, MG - Brazil",
+      "degree": "Bachelors",
+      "major": "Psychology",
+      "date": 2002,
+      "url": "http://www.pucminas.br/destaques/destaques.php"
+    }
+  ],
+
+  "onlineCourses": [
+     {
+       "title": "Python for Everybody",
+       "school" : "University of Michigan - Coursera",
+       "dates": 2016,
+       "specialization": "https://www.coursera.org/account/accomplishments/specialization/34A6VHCWGN7C",
+       "url": "https://www.coursera.org/specializations/python"
+     },
+
+     {
+       "title": "Rais with Active Record and Action Pack",
+       "school" : "John Hopkins University - Coursera",
+       "dates": 2016,
+       "certificate": "https://www.coursera.org/account/accomplishments/certificate/4WJFUPYM7T6N",
+       "url": "https://www.coursera.org/learn/rails-with-active-record/"
+     },
+
+     {
+       "title": "HTML, CSS, and JavaScript for Web Developers",
+       "school" : "John Hopkins University - Coursera",
+       "dates": 2016,
+       "certificate": "https://www.coursera.org/account/accomplishments/certificate/HMT8AWHRTT4U",
+       "url": "https://www.coursera.org/learn/html-css-javascript-for-web-developers/"
+     },
+
+     {
+       "title": "Database Management Essentials",
+       "school" : "University of Colorado - Coursera",
+       "dates": 2016,
+       "certificate": "https://www.coursera.org/account/accomplishments/certificate/A329L8LNUUCU",
+       "url": "https://www.coursera.org/learn/database-management"
+     },
+
+     {
+       "title": "Getting and Cleaning Data",
+       "school" : "John Hopkins University - Coursera",
+       "dates": 2016,
+       "certificate": "https://www.coursera.org/account/accomplishments/certificate/HMT8AWHRTT4U",
+       "url": "https://www.coursera.org/learn/data-cleaning"
+     },
+
+     {
+       "title": "Inferential Statistics",
+       "school" : "University of Amsterdam - Coursera",
+       "dates": 2016,
+       "certificate": "https://www.coursera.org/account/accomplishments/certificate/3844HR2T63DZ",
+       "url": "https://www.coursera.org/learn/inferential-statistics"
+     },
+
+     {
+       "title": "Basic Statistics",
+       "school" : "University of Amsterdam - Coursera",
+       "dates": 2016,
+       "certificate": "https://www.coursera.org/account/accomplishments/certificate/28G9KY6T7REW",
+       "url": "https://www.coursera.org/learn/basic-statistics"
+     },
+
+     {
+       "title": "The Data Scientist's Toolbox",
+       "school" : "John Hopkins University - Coursera",
+       "dates": 2016,
+       "certificate": "https://www.coursera.org/account/accomplishments/certificate/NCTP6QVGW4PN",
+       "url": "https://www.coursera.org/learn/data-scientists-tools"
+     },
+
+     {
+       "title": "R Programming",
+       "school" : "John Hopkins University - Coursera",
+       "dates": 2016,
+       "certificate": "https://www.coursera.org/account/accomplishments/certificate/Q8SZKM92GA9K",
+       "url": "https://www.coursera.org/learn/r-programming"
+     },
+
+     {
+       "title": "Ruby on Rails: An Introduction",
+       "school" : "John Hopkins University - Coursera",
+       "dates": 2016,
+       "certificate": "https://www.coursera.org/account/accomplishments/certificate/YD3GADQ3J9DF",
+       "url": "https://www.coursera.org/learn/ruby-on-rails-intro"
+     },
+
+     {
+       "title": "An Introduction to Interactive Programming in Python (Part 1)",
+       "school" : "Rice University - Coursera",
+       "dates": 2016,
+       "certificate": "https://www.coursera.org/account/accomplishments/certificate/RJMY7YSBTCSG",
+       "url": "https://www.coursera.org/learn/interactive-python-1"
+     },
+
+     {
+       "title": "HTML, CSS and JavaScript",
+       "school" : "The Hong Kong University of Science and Technology - Coursera",
+       "dates": 2016,
+       "certificate": "https://www.coursera.org/account/accomplishments/certificate/C43853F3AQ4V",
+       "url": "https://www.coursera.org/learn/html-css-javascript"
+     },
+}
